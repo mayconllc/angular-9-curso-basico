b9cb497fceafdce93efd1dcb8294032a15c3487e
Maycon.L.L.C <mayconllc@gmail.com>
2020-07-15 17:34:27 -0300

    atualizando titulo da navegação

--- src/app/views/product/product.component.ts
+++ src/app/views/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { HeaderService } from 'src/app/components/template/header/header.service';
 
 @Component({
   selector: 'app-product',
@@ -9,7 +10,12 @@ import { Router } from '@angular/router';
 
 export class ProductComponent {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private headerService: HeaderService) {     
+    headerService.headerData = {
+      title: 'Produtos',
+      icon: 'storefront'
+    }    
+  }
 
   createProduct(){
     this.router.navigate(['/products/create']);
